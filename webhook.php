95361b32b133d0200a58dca1eed69e4d266287bf
kase <kase@kase-no-MacBook-Pro.local>
2020-12-06 00:07:42 +0900

    make15

--- webhook.php
+++ webhook.php
@@ -1,23 +1,5 @@
 <?php
-
-/**
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 require_once('./LINEBotTiny.php');
-
 $channelAccessToken = 'Lpm434bCKVMXwPrWzzXBwlYs9RCFtSVREK4NOCk0MgcJ+yH2dYr0CjGYbdiJNSJ7PvcfENA8cYDHa68/6DI77yuA47wWBLPoxbfWyyB9WpdVwmIY+ZptXxi3azGC1SPmZ7mOEq3X37Kr8V/Ecu3NewdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'c70215b39f1d8d6919dee4819b40a2f7';
 
@@ -33,12 +15,22 @@ foreach ($client->parseEvents() as $event) {
         $message = $event['message'];
         switch ($message['type']) {
             //メッセージタイプがスタンプの場合
+            case 'text':
+                $messages = [
+                    [
+                        'type' => 'text',
+                        'text' => '位置情報を送ってください',
+
+                    ]
+                ];
+                replyMessage($client, $event['replyToken'], $messages);
+                break;
             case 'sticker':
                 $messages = [
                     [
-                        'type' => 'sticker',
-                        'packageId' => 11537,
-                        'stickerId' => 52002739,
+                        'type' => 'text',
+                        'text' => '位置情報を送ってください',
+
                     ]
                 ];
                 replyMessage($client, $event['replyToken'], $messages);
@@ -53,7 +45,7 @@ foreach ($client->parseEvents() as $event) {
                 // アクセスキー
                 $accesskey = '2a60a96fb9488110';
                 //範囲
-                $range = 3;
+                $range = 5;
                 //URL組み立て
                 $url  = $uri . '?key=' . $accesskey . '&lat=' . $lat . '&lng=' . $lng . '&range=' . $range . '&format=json';
                 //apiの情報取得
@@ -95,12 +87,11 @@ foreach ($client->parseEvents() as $event) {
                                         //'defaultAction' => [
                                         //'type' => 'uri',
                                         //'label' =>' View detail',
-                                        //'uri' => 'http://example.com/page/123', //ぐるなびuri
-                                    //],
+                                        //],
                                         'actions' => [
                                             [
                                                 'type' => 'uri',
-                                                'label' => 'ぐるなびサイトへ',
+                                                'label' => 'ホットペッパーサイトへ',
                                                 'uri'=>$columns[0]['actions'][0]['uri'],
                                             ]
                                         ]
@@ -110,15 +101,10 @@ foreach ($client->parseEvents() as $event) {
                                         'imageBackgroundColor' => '#FFFFFF',
                                         'title' => $columns[1][title],
                                         'text' => $columns[1][text],//位置情報から店舗までの経路案内にリンク予定
-                                        //'defaultAction' => [
-                                        //'type' => 'uri',
-                                        //'label' =>' View detail',
-                                        //'uri' => 'http://example.com/page/123', //ぐるなびuri
-                                    //],
                                         'actions' => [
                                             [
                                                 'type' => 'uri',
-                                                'label' => 'ぐるなびサイトへ',
+                                                'label' => 'ホットペッパーサイトへ',
                                                 'uri' => $columns[1]['actions'][0]['uri'],
                                             ]
                                         ]
@@ -128,15 +114,10 @@ foreach ($client->parseEvents() as $event) {
                                         'imageBackgroundColor' => '#FFFFFF',
                                         'title' => $columns[2][title],
                                         'text' => $columns[2][text],//リンクにしたい
-                                        //'defaultAction' => [
-                                        //'type' => 'uri',
-                                        //'label' =>' View detail',
-                                        //'uri' => 'http://example.com/page/123', //ぐるなびuri
-                                    //],
                                         'actions' => [
                                             [
                                                 'type' => 'uri',
-                                                'label' => 'ぐるなびサイトへ',
+                                                'label' => 'ホットペッパーサイトへ',
                                                 'uri' => $columns[2]['actions'][0]['uri'],
                                             ]
                                         ]
@@ -151,7 +132,7 @@ foreach ($client->parseEvents() as $event) {
                         $messages = [
                             [
                                 'type' => 'text',
-                                'text' => '残念ですが近くにラーメン屋が見つかりませんでした。'
+                                'text' => '近くにお店が見つかりませんでした。'
                             ]
                     ];
                     replyMessage($client, $event['replyToken'], $messages);
