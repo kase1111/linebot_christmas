8665d24f5bf9b66795c8cf5bc2f8cd4900428ec0
kase <kase@kase-no-MacBook-Pro.local>
2020-12-13 15:50:17 +0900

    first

--- webhook.php
+++ webhook.php
@@ -1,59 +1,8 @@
 <?php
 require_once('./LINEBotTiny.php');
-$channelAccessToken = 'VXAu/vpj3M5WrcfasKd8+GsWwUjccHBLKRxN3mEg4VKzqbjSe+dJHBmdENpfY3z79kvA63eVRFYnF0YkS9SQcKZEWYzyD+xbq/vIL5spzzdSv2NaRSDnm2/mnfU1Y5YgucKEe4fq0u6Zm4Npp2td/AdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '6c467bf7a3b8ba71de2575038e27c522';
+$channelAccessToken = 'vmHip9gH5zZrT3EDp5FXM2P4bum8MrrsJC9LHmgwIMDvzaF23dtJ57u1wyTcT86mnUM4Bi3QdvekJCO/s9njAnbv/BRGc/D6utFsYhvM3RnDSW3EzMu+3jAqWhyvtgsL/YYrXN8Gc5jnFGgk6ke8/QdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '1d45ac84e0c412bb43fd023ecaa88ed0';
 
-require __DIR__ . '/vendor/autoload.php';
-function getClient()
-{
-    $client = new Google_Client();
-    $client->setApplicationName('Google Sheets API PHP Quickstart');
-    $client->setScopes(Google_Service_Sheets::SPREADSHEETS);
-    $client->setAuthConfig('credentials.json');
-    $client->setAccessType('offline');
-    $client->setPrompt('select_account consent');
-    $tokenPath = 'token.json';
-    if (file_exists($tokenPath)) {
-        $accessToken = json_decode(file_get_contents($tokenPath), true);
-        $client->setAccessToken($accessToken);
-    }
-    if ($client->isAccessTokenExpired()) {
-        if ($client->getRefreshToken()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        } else {
-            $authUrl = $client->createAuthUrl();
-            printf("Open the following link in your browser:\n%s\n", $authUrl);
-            print 'Enter verification code: ';
-            $authCode = trim(fgets(STDIN));
-
-            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-            $client->setAccessToken($accessToken);
-
-            if (array_key_exists('error', $accessToken)) {
-                throw new Exception(join(', ', $accessToken));
-            }
-        }
-        if (!file_exists(dirname($tokenPath))) {
-            mkdir(dirname($tokenPath), 0700, true);
-        }
-        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-    }
-    return $client;
-}
-$client = getClient();
-$service = new Google_Service_Sheets($client);
-
-$spreadsheetId = '1RObht-7A9jEDU_a8z_8tcboIEVi-aWD0Wjacih3G1ZM';
-$range = 'Sheet1!B2:C4';
-$response = $service->spreadsheets_values->get($spreadsheetId, $range);
-$values = $response->getValues();
-$clients = new LINEBotTiny($channelAccessToken, $channelSecret);
-function replyMessage($clients, $reply_token, $messages) {
-    return $clients->replyMessage([
-        'replyToken' => $reply_token,
-        'messages' => $messages
-    ]);
-}
 foreach ($clients->parseEvents() as $event) {
     if ($event['type'] == 'message') {
         $message = $event['message'];
