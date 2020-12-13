1cc61f254d6ab5224c63ec8fd93e7bbc89cf6517
kase <kase@kase-no-MacBook-Pro.local>
2020-12-13 16:03:41 +0900

    3

--- webhook.php
+++ webhook.php
@@ -2,7 +2,13 @@
 require_once('./LINEBotTiny.php');
 $channelAccessToken = 'vmHip9gH5zZrT3EDp5FXM2P4bum8MrrsJC9LHmgwIMDvzaF23dtJ57u1wyTcT86mnUM4Bi3QdvekJCO/s9njAnbv/BRGc/D6utFsYhvM3RnDSW3EzMu+3jAqWhyvtgsL/YYrXN8Gc5jnFGgk6ke8/QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '1d45ac84e0c412bb43fd023ecaa88ed0';
-
+$clients = new LINEBotTiny($channelAccessToken, $channelSecret);
+function replyMessage($clients, $reply_token, $messages) {
+    return $clients->replyMessage([
+        'replyToken' => $reply_token,
+        'messages' => $messages
+    ]);
+}
 foreach ($clients->parseEvents() as $event) {
     if ($event['type'] == 'message') {
         $message = $event['message'];
