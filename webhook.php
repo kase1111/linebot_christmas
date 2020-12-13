aadac6cadbab27d88cfa0ba4866137c7ce1bea3f
kase <kase@kase-no-MacBook-Pro.local>
2020-12-13 19:36:15 +0900

    button 分岐３つ

--- webhook.php
+++ webhook.php
@@ -85,7 +85,7 @@ foreach ($clients->parseEvents() as $event) {
                             "altText" => "This is a buttons template",
                             "template" => [
                                 "type" => "buttons",
-                                "thumbnailImageUrl" => "https://profile.line-scdn.net/0m0201800c7251db82f85e7e2f9dec6ae822521f83cae0",
+                                "thumbnailImageUrl" => "https://www.illust-box.jp/db_img/sozai/00012/123501/watermark.jpg",
                                 "imageAspectRatio" => "rectangle",
                                 "imageSize" => "cover",
                                 "imageBackgroundColor" => "#FFFFFF",
